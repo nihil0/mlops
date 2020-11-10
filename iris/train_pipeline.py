bbfc63e5c297c1dd661c615761024d0834cefeb6
Neelabh Kashyap <neelabh2@gmail.com>
2020-11-10 16:41:55 +0200

    changed model tag

--- iris/train_pipeline.py
+++ iris/train_pipeline.py
@@ -130,7 +130,7 @@ register_model = PythonScriptStep(
         "--tag",
         "team='Data Science'",
         "--tag",
-        "comment=qa",
+        "comment=prod",
     ],
     inputs=[model_outpath],
     compute_target=compute_target,
